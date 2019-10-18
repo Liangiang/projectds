c4bc9b65baa2b2f2076dfadc6ecbede65745fce9
Lx <liangxiu@htah.com.cn>
2019-10-18 09:43:11 +0800

    pom文件修改

--- src/main/java/com/ex/project/ProjectApplication.java
+++ src/main/java/com/ex/project/ProjectApplication.java
@@ -1,10 +1,19 @@
 package com.ex.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
+@MapperScan("com.ex.project.mapper")
 public class ProjectApplication {
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
--- src/main/java/com/ex/project/service/impl/UserServiceImpl.java
+++ src/main/java/com/ex/project/service/impl/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.ex.project.service.impl;
 
 import com.ex.project.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
+
 }
