3262f7ee057af2cec377b9a3562b42d73108c8e6
Lx <liangxiu@htah.com.cn>
2020-02-03 00:06:49 +0800

    修改订单状态接口变化，订单状态修改时，对用户的钱进行增减

--- src/main/java/com/ex/project/service/impl/OrderInfoServiceImpl.java
+++ src/main/java/com/ex/project/service/impl/OrderInfoServiceImpl.java
@@ -2,12 +2,17 @@ package com.ex.project.service.impl;
 
 import com.ex.project.mapper.GoodsInfoMapper;
 import com.ex.project.mapper.OrderInfoMapper;
+import com.ex.project.mapper.UserInfoMapper;
 import com.ex.project.model.GoodsInfo;
 import com.ex.project.model.OrderInfo;
+import com.ex.project.model.UserInfo;
 import com.ex.project.service.OrderInfoService;
+import com.ex.project.untill.CodeConsts;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -19,6 +24,8 @@ public class OrderInfoServiceImpl implements OrderInfoService {
     private OrderInfoMapper orderInfoMapper;
     @Resource
     private GoodsInfoMapper goodsInfoMapper;
+    @Resource
+    private UserInfoMapper userInfoMapper;
 
     @Override
     public int add_order(OrderInfo orderInfo) {
@@ -36,7 +43,23 @@ public class OrderInfoServiceImpl implements OrderInfoService {
 
     @Override
     public int up_order(OrderInfo orderInfo) {
-        int returnData = orderInfoMapper.updateByPrimaryKeySelective(orderInfo);
+        int flag = CodeConsts.ZT_SB;
+        int returnData = CodeConsts.ZT_SB;
+        switch (orderInfo.getoStatus()) {
+            //已付款
+            case CodeConsts.DDZT_YFK:
+                flag = ChangeMoney(orderInfo.getoId(), orderInfo.getoStatus());
+                break;
+            //6同意退货
+            case CodeConsts.DDZT_TYTH:
+                flag = ChangeMoney(orderInfo.getoId(), orderInfo.getoStatus());
+                break;
+            default:
+                flag = 1;
+        }
+        if (flag == CodeConsts.ZT_CG) {
+            returnData = orderInfoMapper.updateByPrimaryKeySelective(orderInfo);
+        }
         return returnData;
     }
 
@@ -53,5 +76,46 @@ public class OrderInfoServiceImpl implements OrderInfoService {
         PageInfo<OrderInfo> returnData = new PageInfo<>(a);
         return returnData;
     }
+
+    /**
+     * @return int
+     * @Author lx
+     * @Description 改变钱
+     * @Date 22:38 2020/2/2
+     * @Param [oId]
+     **/
+    @Transactional(rollbackFor = Exception.class)
+    public int ChangeMoney(Integer oId, Integer oStatus) {
+        try {
+            OrderInfo orderInfo = orderInfoMapper.selectByPrimaryKey(oId);
+            Double countPrice = orderInfo.getCountPrice();
+            Integer uId = orderInfo.getuId();
+            UserInfo userInfo = userInfoMapper.selectByPrimaryKey(orderInfo.getuId());
+            Double beforeMoney = userInfo.getMoney();
+            Double UserAfterMoney = 0.0;
+            Double ShopAfterMoney = 0.0;
+            Double ShopMoney = 0.0;
+            switch (oStatus) {
+                case CodeConsts.DDZT_YFK:
+                    UserAfterMoney = userInfo.getMoney() - orderInfo.getCountPrice();
+                    ShopAfterMoney = ShopMoney + orderInfo.getCountPrice();
+                    break;
+                case CodeConsts.DDZT_TYTH:
+                    UserAfterMoney = userInfo.getMoney() + orderInfo.getCountPrice();
+                    ShopAfterMoney = ShopMoney - orderInfo.getCountPrice();
+                    break;
+            }
+            userInfo.setMoney(UserAfterMoney);
+
+            int upUser = userInfoMapper.updateByPrimaryKeySelective(userInfo);
+
+            return upUser;
+        } catch (Exception e) {
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            return 0;
+
+        }
+    }
+
 
 }
---
+++ src/main/java/com/ex/project/untill/CodeConsts.java
@@ -0,0 +1,55 @@
+package com.ex.project.untill;
+
+public class CodeConsts {
+    /**
+     * @Author lx
+     * @Description mapper成功
+     **/
+    public static final int ZT_CG=1;
+    /**
+     * @Author lx
+     * @Description mapper失败
+     **/
+    public static final int ZT_SB=0;
+    /**
+     * @Author lx
+     * @Description 订单状态-0下订单
+     **/
+    public static final int DDZT_WZF=0;
+    /**
+     * @Author lx
+     * @Description 订单状态-1已付款
+     **/
+    public static final int DDZT_YFK=1;
+    /**
+     * @Author lx
+     * @Description 订单状态-2取消订单
+     **/
+    public static final int DDZT_QXDD=2;
+    /**
+     * @Author lx
+     * @Description 订单状态-3已发货
+     **/
+    public static final int DDZT_YFH=3;
+    /**
+     * @Author lx
+     * @Description 订单状态-4已收货
+     **/
+    public static final int DDZT_YSH=4;
+    /**
+     * @Author lx
+     * @Description 订单状态-5申请退货
+     **/
+    public static final int DDZT_SQTH=5;
+    /**
+     * @Author lx
+     * @Description 订单状态-6同意退货
+     **/
+    public static final int DDZT_TYTH=6;
+    /**
+     * @Author lx
+     * @Description 订单状态-7拒绝退货
+     **/
+    public static final int DDZT_JJTH=7;
+
+}
